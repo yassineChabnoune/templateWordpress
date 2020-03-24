5e32b144053401c62aa32f6884abff5ead264121
yassineChabnoune <yasine.chabnoune@gmail.com>
2020-03-24 21:56:04 +0000

    hkjh

--- functions.php
+++ functions.php
@@ -1,12 +1,16 @@
-<?php 
-///ENABLE MENU OPTIONS
 
-add_theme_support( 'menus' );
+
+<?php
+///enable menu options
+
+add_theme_support('menus');
 
 //REGISTER MENU
 function set_menu_areas()
 {
-	register_nav_menu( 'main-menu' , 'primary-menu' );
+    register_nav_menu('main-menu','Primary Menu');
 }
-add_action( 'init', 'set_menu_areas')
- ?>
+add_action('init','set_menu_areas');
+
+
+    ?>
