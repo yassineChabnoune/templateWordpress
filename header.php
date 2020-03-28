b64f2f21e9432979a661374421008ee99d0a575c
iliasattar <iliasattar16@gmail.com>
2020-03-28 19:25:46 +0100

    commit avant finale

--- header.php
+++ header.php
@@ -11,10 +11,54 @@
     <?php wp_head(); ?>
 </head>
 <body>
+
+<!-- test -->
+
     
+
+
     <div class="container" >
     	<header class="site-header">
-    		<h1><a href="<?php echo home_url(); ?>"><img src='EST-Sidi-bennour.png'></a></h1>
+    		
+    		<div id="carouselId" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselId" data-slide-to="1"></li>
+        <li data-target="#carouselId" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img style="width:100%; height:70vh"  src="<?php echo get_template_directory_uri()?>/image/img1.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3 style>DUT Génie Informatique </h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img style="width:100%; height:70vh"  src="<?php echo get_template_directory_uri()?>/image/img2.jpg" alt="Second slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>DUT technique de Management</h3>
+                <p>Description</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img style="width:100%; height:70vh" src="<?php echo get_template_directory_uri()?>/image/img3.jpg" alt="Third slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>DUT GA</h3>
+                <p>Description</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
     	</header>
     	<nav class="main-nav">
     		<?php wp_nav_menu( array('theme_location' => 'main-menu') ); ?>
